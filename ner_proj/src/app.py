fe85f912a57477b3a093a94c02ed780b56f85a21
natalyasegal <71938116+natalyasegal@users.noreply.github.com>
2020-09-28 18:22:44 +0300

    Delete app.py

--- ner_proj/src/app.py
+++
@@ -1,8 +0,0 @@
-import spacy
-
-#nlp = spacy.load('en_core_web_sm')
-nlp = spacy.load("/app/en_model-0.0.0/en_model/en_model-0.0.0/")
-#nlp = spacy.load("ns_models2/en_model-0.0.0/en_model/en_model-0.0.0/")
-doc = nlp(u'Ronald just bought 2 shares at 9 a.m. because the stock went up 30% in just 2 days according to the WSJ. Us is a country in north America')
-for ent in doc.ents:
-    print(ent.text, ent.label_)
